cacfb57c074ca779d7fa1e2bcd5faff1bbd93aa4
Jakub Woźny <jakub2787@gmail.com>
2023-06-09 15:40:21 +0200

    added react router

---
+++ src/core/App.js
@@ -0,0 +1,12 @@
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom';
+import Navigation from '../common/Navigation';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+export const App = () => (
+  <HashRouter>
+    <Navigation />
+    <Switch>
+    </Switch>
+  </HashRouter>
+);
+
