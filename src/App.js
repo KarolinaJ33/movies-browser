31f443f71634247cf3fea3fcba714061da5468aa
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-09 16:03:12 +0200

    update the address for component import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme'; 
-import MoviesBrowser from '././features/MoviesBrowser/MovieDetails';
+import MoviesBrowser from '././features/MoviesBrowser/MoviePage/MovieTile';
 
 function App() {
 
