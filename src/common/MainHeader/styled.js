668e391b00b10f2dbf4cbc579baacdb6cc259fa3
Marzena Malka <marzena.malka@gmail.com>
2023-06-12 20:21:08 +0200

    added NoResult page

---
+++ src/common/MainHeader/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledMainHeader = styled.h`
+    font-size: 36px;
+    font-weight: 600;
+    margin-top: 56px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 24px;
+    margin-top: 24px;
+    padding-left: 24px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
+    font-size: 18px;
+    margin-top: 24px;
+    padding-left: 24px;
+  }
+`
