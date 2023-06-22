baaeb007c3cf2eaf708b9b934819ef90f7e8020c
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-06-22 13:10:44 +0200

    added component SearchResult

---
+++ src/common/SearchResult/index.js
@@ -0,0 +1,10 @@
+import { Container, Info, ResultList } from "./styled";
+
+export const SearchResult = ({ title }) => {
+    return (
+        <Container>
+            <Info>Search result for{title}</Info>
+            <ResultList></ResultList>
+        </Container>
+    );
+};
