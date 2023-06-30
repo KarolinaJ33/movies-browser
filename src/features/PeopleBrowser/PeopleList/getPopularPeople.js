ba73e1f0ac92e012315e7b5c7c53c9aaa2b94877
Marzena Malka <marzena.malka@gmail.com>
2023-06-30 21:18:07 +0200

    added Pagination on People

--- src/features/PeopleBrowser/PeopleList/getPopularPeople.js
+++ src/features/PeopleBrowser/PeopleList/getPopularPeople.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { API_KEY, API_URL } from "../../../core/App/apiCodes";
+import { API_KEY, ApiPopularPeople } from "../../../core/App/apiCodes";
 
-export const getPopularPeople = async ({ page }) => {
+export const getPopularPeople = async ({ currentPage }) => {
     const response = await axios.get(
-      `${API_URL}/person/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+      `${ApiPopularPeople}?api_key=${API_KEY}&language=en-US&page=${currentPage}`
     );
     if (!response.ok) {
       new Error(response.statusText);
