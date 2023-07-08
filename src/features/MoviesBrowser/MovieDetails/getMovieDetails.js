1420ecfd641df8784e1b11ecaf154069abb02f7e
Jakub Woźny <jakub2787@gmail.com>
2023-07-09 00:58:01 +0200

    adding credits to MovieDetails

--- src/features/MoviesBrowser/MovieDetails/getMovieDetails.js
+++ src/features/MoviesBrowser/MovieDetails/getMovieDetails.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_KEY = "ae7d4af255a05506e1ed3b49e48b0d5c";
 const API_URL = "https://api.themoviedb.org/3";
 
-export const getMovieDetails = async ( movieId ) => {
+export const getMovieDetails = async (movieId) => {
   try {
     const response = await axios.get(
       `${API_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
--- src/features/MoviesBrowser/MovieDetails/index.js
+++ src/features/MoviesBrowser/MovieDetails/index.js
@@ -1,18 +1,24 @@
+import { Bottom, Container, List, StyledLink, Wrapper } from "./styled";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovieDetailsLoading, selectDetails, selectStatus, } from "./movieDetailsSlice";
+import { fetchMovieDetailsLoading, selectCast, selectCrew, selectDetails, selectStatus, } from "./movieDetailsSlice";
 import { Loading } from "../../../common/Loading";
 import Error from "../../../common/Error";
 import { MovieTile } from "./MovieDetailsPage/MovieTile";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import Backdrop from "./MovieDetailsPage/Backdrop";
+import { toPerson } from "../../../core/App/routes";
+import { PersonTile } from "../../../common/PersonTile";
+import { MainHeader } from "../../../common/MainHeader";
 
 const MovieDetails = () => {
     const dispatch = useDispatch();
     const { movieId } = useParams();
     const movieDetails = useSelector(selectDetails);
+    const cast = useSelector((state) => selectCast(state));
+    const crew = useSelector((state) => selectCrew(state));
+    const status = useSelector(selectStatus);
 
-    const status = useSelector(selectStatus)
     useEffect(() => {
         dispatch(fetchMovieDetailsLoading({ id: movieId }));
     }, [dispatch, movieId]);
@@ -26,7 +32,7 @@ const MovieDetails = () => {
     }
 
     return (
-        <div>
+        <>
             {movieDetails && movieDetails.backdrop_path &&
                 <Backdrop
                     original_title={movieDetails.original_title}
@@ -35,19 +41,63 @@ const MovieDetails = () => {
                     vote_count={movieDetails.vote_count}
                 />
             }
-            {movieDetails && movieDetails.title && (
-                <MovieTile
-                    poster_path={movieDetails.poster_path}
-                    title={movieDetails.title}
-                    release={movieDetails.release_date}
-                    production={movieDetails.production_countries}
-                    genres={movieDetails.genres}
-                    rate={movieDetails.vote_average}
-                    votes={movieDetails.vote_count}
-                    details={movieDetails.overview}
-                />
-            )}
-        </div>
+            <Container>
+                {movieDetails && movieDetails.title && (
+                    <MovieTile
+                        poster_path={movieDetails.poster_path}
+                        title={movieDetails.title}
+                        release={movieDetails.release_date}
+                        production={movieDetails.production_countries}
+                        genres={movieDetails.genres}
+                        rate={movieDetails.vote_average}
+                        votes={movieDetails.vote_count}
+                        details={movieDetails.overview}
+                    />
+                )}
+                {cast.length > 0 && (
+                    <>
+                        <Wrapper>
+                            <MainHeader title={`Movies - cast (${cast.length})`} />
+                            <List>
+                                {cast.map((person, index) => (
+                                    <div key={`$person.id-${index}`}>
+                                        <StyledLink to={toPerson({ personId: person.id })}>
+                                            <PersonTile
+                                                id={person.id}
+                                                name={person.name}
+                                                poster={person.profile_path}
+                                                role={person.character}
+                                            />
+                                        </StyledLink>
+                                    </div>
+                                ))}
+                            </List>
+                        </Wrapper>
+                    </>
+                )}
+                {crew.length > 0 && (
+                    <>
+                        <Wrapper>
+                            <MainHeader title={`Movies - crew (${crew.length})`} />
+                            <List>
+                                {crew.map((person, index) => (
+                                    <div key={`$person.id-${index}`}>
+                                        <StyledLink to={toPerson({ personId: person.id })}>
+                                            <PersonTile
+                                                id={person.id}
+                                                name={person.name}
+                                                role={person.job}
+                                                poster={person.profile_path}
+                                            />
+                                        </StyledLink>
+                                    </div>
+                                ))}
+                            </List>
+                        </Wrapper>
+                    </>
+                )}
+                <Bottom />
+            </Container></>
     )
 };
 
